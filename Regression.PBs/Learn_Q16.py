f4254279d1aadca408a4a4cbf45aef0da2c97287
Yury Milchevskiy <92919987+Milchevskiy@users.noreply.github.com>
2023-10-17 15:45:56 +0300

    Update Learn_Q16.py

--- Regression.PBs/Learn_Q16.py
+++ Regression.PBs/Learn_Q16.py
@@ -136,7 +136,7 @@ print(yt)
 print(Xt.shape)
 print(yt.shape)
 
-if test_enabled == 1
+if test_enabled == 1:
     X = numpy.load("test_Q16X.npy")
     y = numpy.load("test_Q16Y.npy")
 
@@ -213,7 +213,7 @@ y_pred_test_1 = model(Xtest_1)
 q16=accuracy(ytest_1, y_pred_test_1)
 printf("Final Q16 for CB513 %lf\n", q16)
 
-if test_enabled == 1
+if test_enabled == 1:
     y_pred_test = model(Xtest)
     q16t=accuracy(ytest, y_pred_test)
     printf("Final Q16 for test sample %lf\n",q16t)
